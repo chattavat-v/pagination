61fae83f2ba3d4f44c2a7628c57e57f75d11aa15
chattavat.macOs <chattavat.v@gmail.com>
2019-10-20 23:32:18 +0700

    build model blog

---
+++ models/Blog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const BlogSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  body: {
+    type: String
+  },
+  image_url: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Blog = mongoose.model('blog', BlogSchema);
